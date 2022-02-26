1513fa31f0c47d4abe1591f3ba1b032dcde1381d
Babafemi Ogundipe <smiles2k6@gmail.com>
2022-02-26 18:06:44 +0100

    with parameters

--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -4,73 +4,28 @@ using System.Linq;
 
 namespace PlayGround
 {
-    internal class Program
+    [Me("25","Femi")]
+     class Program
     {
 
         static void Main(string[] args)
         {
 
-            /*            
-             Get all test  attributes
-             */
-            var testSuites = from d in Assembly.GetExecutingAssembly().GetTypes()
-                             where d.GetCustomAttributes().Any(a => a is TestAttribute)
-                             select d;
-
-            foreach (Type attributeType in testSuites)
-            {
-                /*            
-             Get all method attributes
-             */
-                var getTestMethods = from d in attributeType.GetMethods()
-                                     where d.GetCustomAttributes().Any(m => m is TestMethodAttribute)
-                                     select d;
-
-                /* dynamically instantiate the object*/
-                var createInstance = Activator.CreateInstance(attributeType);
-
-                foreach (MethodInfo methodInfo in getTestMethods)
-                {
-                    /* dynamically call the methods*/
-                    methodInfo.Invoke(createInstance, new object[0]);
-                }
-
-
-            }
+             typeof(Program).GetCustomAttributes();
 
         }
     }
 
-    sealed class TestAttribute : Attribute
+
+    class MeAttribute : Attribute
     {
-
-    }
-
-    public class TestMethodAttribute : Attribute
-    {
-
-    }
-    [TestAttribute]
-    public class MyTestAttribute
-    {
-        public void HelperMethod()
+        public MeAttribute(string value, string secondValue)
         {
-
-        }
-        [TestMethodAttribute]
-        public void Method1()
-        {
-            Console.WriteLine("Method 1 works");
-            HelperMethod();
-        }
-        [TestMethodAttribute]
-        public void Method2()
-        {
-            Console.WriteLine("Method 2 works");
-            HelperMethod();
+            Console.WriteLine("I Work");
+            Console.WriteLine(value);
+            Console.WriteLine(secondValue);
         }
     }
-
 
 
 
